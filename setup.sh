7fa954984edbe6ad86f5e95282b704f7d0331337
DefKorns <defkorns@gmail.com>
2022-01-25 09:08:02 -0100

    passing arguments to script + help

--- setup.sh
+++ setup.sh
@@ -7,11 +7,14 @@ export PATH=$OPENOCD:$PATH
 
 # VARIABLES
 #For best compatibility, please use Ubuntu 20.04 (0.10.0) or greater!
+adpArgs=("jlink" "rpi" "stlink")
+chpArg=("4" "64" "128" "256" "512" "4Mb" "64Mb" "128Mb" "256Mb" "512Mb")
+devArg=("mario" "zelda")
 adapters=("J-Link" "Raspberry Pi" "ST-LINK" "Quit")
 packages=("binutils-arm-none-eabi" "python3" "libhidapi-hidraw0" "libftdi1" "libftdi1-2" "git" "make" "python3-pip")
 openocd=(openocd-git_*_amd64.deb)
 options=("Backup & Restore Tools" "Retro-Go" "Custom Firmware" "Exit")
-chips=("4Mb" "64Mb" "Quit")
+chips=("4Mb" "64Mb" "128Mb" "256Mb" "512Mb" "Quit")
 gwVersion=("Mario" "Zelda" "Quit")
 cfwDir="$PWD/game-and-watch-patch"
 backupDir="$PWD/game-and-watch-backup/backups"
@@ -32,92 +35,50 @@ errorMsg() {
   exit
 }
 
+helptext() {
+  echo "Usage: $0 <Adapter: jlink or stlink or rpi> <mario or zelda> <ChipSize: 4Mb or 64Mb or 128Mb or 256Mb or 512Mb>"
+}
+
+optionsDefaultValue() {
+  local item i=0 numItems=$#
+
+  # Print numbered menu items, based on the arguments passed.
+  for item; do # Short for: for item in "$@"; do
+    printf '%s\n' "$((++i))) $item"
+  done >&2 # Print to stderr, as `select` does.
+
+  # Prompt the user for the index of the desired item.
+  while :; do
+    printf %s "${PS3-#? }" >&2 # Print the prompt string to stderr, as `select` does.
+    read -r index
+    # Make sure that the input is either empty or that a valid index was entered.
+    [[ -z $index ]] && break # empty input
+    ((index >= 1 && index <= numItems)) 2>/dev/null || {
+      echo "Invalid selection. Please try again." >&2
+      continue
+    }
+    break
+  done
+
+  # Output the selected item, if any.
+  [[ -n $index ]] && printf %s "${@:index:1}"
+
+}
+
 retroGo() {
+  cd game-and-watch-retro-go || exit
+  romChecker
+  make clean
+
+  OPENOCD="/opt/openocd-git/bin/openocd"
+  GCC_PATH="../gcc-arm-none-eabi-10.3-2021.10/bin"
   [ "$DEVICE" == "mario" ] && make -j"$(nproc)" INTFLASH_BANK=2 flash
 
   if [ "$DEVICE" == "zelda" ]; then
-    [ "$CHIP" == "4Mb" ] && make -j"$(nproc)" INTFLASH_BANK=2 EXTFLASH_SIZE=1802240 EXTFLASH_OFFSET=851968 GNW_TARGET=zelda EXTENDED=1 flash
-    [ "$CHIP" == "64Mb" ] && make -j"$(nproc)" EXTFLASH_SIZE_MB=60 EXTFLASH_OFFSET=4194304 INTFLASH_BANK=2 flash
+    [ "$CHIP" == "4" ] && make -j"$(nproc)" INTFLASH_BANK=2 EXTFLASH_SIZE=1802240 EXTFLASH_OFFSET=851968 GNW_TARGET=zelda EXTENDED=1 flash
+    [ "$CHIP" -ge "64" ] && make -j"$(nproc)" EXTFLASH_SIZE_MB="$CHIP" EXTFLASH_OFFSET=4194304 INTFLASH_BANK=2 flash
   fi
-}
-
-toolsStep() {
-  clear
-  PS3='Please select what do you want to do: '
-  echo ""
-  select opt in "${options[@]}"; do
-    case $opt in
-    "Backup & Restore Tools")
-      echo "Before continue please check the correct pinout https://imgur.com/6IxzPi9. Press return when ready!"
-      read -rn 1
-      echo "Running Backup & Restore Tools Sanity Checks:"
-      echo ""
-      cd game-and-watch-backup || exit
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      ./1_sanity_check.sh "$ADAPTER" "$DEVICE"
-      echo ""
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      ./2_backup_flash.sh "$ADAPTER" "$DEVICE"
-      echo ""
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      ./3_backup_internal_flash.sh "$ADAPTER" "$DEVICE"
-      echo ""
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      ./4_unlock_device.sh "$ADAPTER" "$DEVICE"
-      echo ""
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      ./5_restore.sh "$ADAPTER" "$DEVICE"
-      cd ..
-      ;;
-    "Retro-Go")
-      echo "Running Retro-Go:"
-      echo ""
-
-      cd game-and-watch-retro-go || exit
-      romChecker
-      make clean
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      GCC_PATH="../gcc-arm-none-eabi-10.3-2021.10/bin"
-      retroGo
-      cd ..
-      exit
-      ;;
-    "Custom Firmware")
-      echo "Custom firmware for the newer Nintendo Game and Watch consoles."
-      echo ""
-
-      [ ! -f "$backupDir/internal_flash_backup_${DEVICE}.bin" ] || [ ! -f "$backupDir/flash_backup_${DEVICE}.bin" ] && errorMsg
-
-      [ ! -f "$cfwDir/flash_backup_${DEVICE}.bin" ] && cp "$backupDir/flash_backup_${DEVICE}.bin" "$cfwDir/flash_backup_${DEVICE}.bin"
-      [ ! -f "$cfwDir/internal_flash_backup_${DEVICE}.bin" ] && cp "$backupDir/internal_flash_backup_${DEVICE}.bin" "$cfwDir/internal_flash_backup_${DEVICE}.bin"
-
-      cd game-and-watch-patch || exit
-
-      make clean
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      [ "$DEVICE" == "mario" ] && make PATCH_PARAMS="--device=mario --internal-only" ADAPTER="$ADAPTER" flash_patched
-
-      if [ "$DEVICE" == "zelda" ]; then
-        [ "$CHIP" == "4Mb" ] && make PATCH_PARAMS="--device=zelda --extended --no-la --no-sleep-images --extended" ADAPTER="$ADAPTER" flash
-        [ "$CHIP" == "64Mb" ] && make PATCH_PARAMS="--device=zelda" ADAPTER="$ADAPTER" LARGE_FLASH=1 flash_patched
-      fi
-
-      cd ..
-
-      cd game-and-watch-retro-go || exit
-      romChecker
-      make clean
-      OPENOCD="/opt/openocd-git/bin/openocd"
-      GCC_PATH="../gcc-arm-none-eabi-10.3-2021.10/bin"
-      retroGo
-      exit
-      ;;
-    "Exit")
-      exit
-      ;;
-    *) echo "invalid option $REPLY" ;;
-    esac
-  done
+  cd ..
 }
 
 romChecker() {
@@ -149,108 +110,6 @@ romChecker() {
   fi
 }
 
-optionsDefaultValue() {
-  local item i=0 numItems=$#
-
-  # Print numbered menu items, based on the arguments passed.
-  for item; do # Short for: for item in "$@"; do
-    printf '%s\n' "$((++i))) $item"
-  done >&2 # Print to stderr, as `select` does.
-
-  # Prompt the user for the index of the desired item.
-  while :; do
-    printf %s "${PS3-#? }" >&2 # Print the prompt string to stderr, as `select` does.
-    read -r index
-    # Make sure that the input is either empty or that a valid index was entered.
-    [[ -z $index ]] && break # empty input
-    ((index >= 1 && index <= numItems)) 2>/dev/null || {
-      echo "Invalid selection. Please try again." >&2
-      continue
-    }
-    break
-  done
-
-  # Output the selected item, if any.
-  [[ -n $index ]] && printf %s "${@:index:1}"
-
-}
-
-chipSize() {
-  echo ""
-  echo "Select the size of your flash chip.
-4 is the default:"
-  echo ""
-
-  chp=$(optionsDefaultValue "${chips[@]}")
-
-  case $chp in
-  "" | "4Mb")
-    CHIP="4"
-    ;;
-  "64Mb")
-    CHIP="64"
-    ;;
-  "Quit")
-    exit
-    ;;
-  esac
-  echo ""
-  echo "You selected a $CHIP Flash Chip"
-  selectDebugger
-}
-
-selectDebugger() {
-  echo ""
-  echo "You must configure this for the debug adapter you're using!
-stlink is the default:"
-  echo ""
-  adp=$(optionsDefaultValue "${adapters[@]}")
-
-  case $adp in
-  "J-Link")
-    ADAPTER="jlink"
-    ;;
-  "Raspberry Pi")
-    ADAPTER="rpi"
-    ;;
-  "" | "ST-LINK")
-    ADAPTER="stlink"
-    ;;
-  "Quit")
-    exit
-    ;;
-  esac
-  echo ""
-  echo "You choose $ADAPTER as your ARM debug probe"
-  toolsStep
-}
-
-selectGWType() {
-  echo ""
-  echo "Please select your Game & Watch type.
-Mario is the default:"
-  echo ""
-
-  gw=$(optionsDefaultValue "${gwVersion[@]}")
-
-  case $gw in
-  "" | "Mario")
-    DEVICE="mario"
-    ;;
-  "Zelda")
-    DEVICE="zelda"
-    chipSize
-    ;;
-  "Quit")
-    exit
-    ;;
-  *) echo "invalid option $REPLY" ;;
-  esac
-  echo ""
-  echo "You choose $DEVICE as your Game & Watch"
-  selectDebugger
-}
-
 getRequirements() {
   ## Run the installDependencies function if any of the libraries are missing.
   dpkg -s "${packages[@]}" >/dev/null 2>&1 || installDependencies
@@ -270,8 +129,6 @@ getRequirements() {
   if [ ! -d "game-and-watch-backup" ]; then
     echo "Cloning and building the Backup & Restore Tools:"
     git clone https://github.com/ghidraninja/game-and-watch-backup
-    cd game-and-watch-backup || exit
-    cd ..
   fi
 
   if [ ! -d "game-and-watch-patch" ]; then
@@ -291,60 +148,210 @@ getRequirements() {
     echo "Cloning and building Retro-Go"
     git clone --recurse-submodules https://github.com/kbeckmann/game-and-watch-retro-go
   fi
+  rm -r "remove_me_after_first_run"
 }
 
-getRequirements
+selectGWType() {
+  selection="${1,,}"
 
-if [ -n "${1}" ]; then
-  case "${1}" in
-  "jlink" | "rpi" | "stlink")
-    ADAPTER="${1}"
-    ;;
-  *)
-    exit
-    ;;
-  esac
-fi
-
-if [ -n "${2}" ]; then
-  case "${2}" in
-  "mario")
+  case "$selection" in
+  "" | "mario")
     DEVICE="mario"
+    GNW="Mario"
+    CHIP="4"
     ;;
   "zelda")
     DEVICE="zelda"
-    [ -z "${3}" ] && chipSize "$DEVICE"
+    GNW="Zelda"
+    [ -z "${2}" ] && chipSelectionMenu
     ;;
-  *)
+  "quit")
+    exit
+    ;;
+  *) echo "invalid option $REPLY" ;;
+  esac
+}
+
+selectDebugger() {
+  adpSelection="${1,,}"
+
+  case $adpSelection in
+  "j-link" | "jlink")
+    ADAPTER="jlink"
+    DEBUGGER="J-Link"
+    ;;
+  "raspberry Pi" | "rpi")
+    ADAPTER="rpi"
+    DEBUGGER="Raspberry Pi"
+    ;;
+  "" | "st-link" | "stlink")
+    ADAPTER="stlink"
+    DEBUGGER="ST-LINK"
+    ;;
+  "quit")
     exit
     ;;
   esac
+}
+
+chipSize() {
+  chpSelection="${1,,}"
+  case "$chpSelection" in
+  "" | "4mb" | "4")
+    CHIP="4"
+    ;;
+  "64mb" | "64")
+    CHIP="64"
+    ;;
+  "128mb" | "128")
+    CHIP="128"
+    ;;
+  "256mb" | "256")
+    CHIP="256"
+    ;;
+  "512mb" | "512")
+    CHIP="512"
+    ;;
+  "quit")
+    exit
+    ;;
+  esac
+  [ "$CHIP" -gt "4" ] && CHIP=$((CHIP - 4))
+}
+
+toolsSelectionMenu() {
+  # clear
+  PS3='Please select what do you want to do: '
+
+  echo ""
+  echo "#################################################################################################"
+  echo "     You're using a ${DEBUGGER} adapter on a Game and Watch ${GNW} version with $((CHIP + 4))Mb chip "
+  echo "#################################################################################################"
+  echo ""
+
+  select opt in "${options[@]}"; do
+    case $opt in
+    "Backup & Restore Tools")
+      echo "Before continue please check the correct pinout https://imgur.com/6IxzPi9. Press return when ready!"
+      read -rn 1
+      echo "Running Backup & Restore Tools Sanity Checks:"
+      echo ""
+      cd game-and-watch-backup || exit
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      ./1_sanity_check.sh "$ADAPTER" "$DEVICE"
+      echo ""
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      ./2_backup_flash.sh "$ADAPTER" "$DEVICE"
+      echo ""
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      ./3_backup_internal_flash.sh "$ADAPTER" "$DEVICE"
+      echo ""
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      ./4_unlock_device.sh "$ADAPTER" "$DEVICE"
+      echo ""
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      ./5_restore.sh "$ADAPTER" "$DEVICE"
+      cd ..
+      ;;
+    "Retro-Go")
+      echo "Running Retro-Go:"
+      echo ""
+      retroGo
+      exit
+      ;;
+    "Custom Firmware")
+      echo "Custom firmware for the newer Nintendo Game and Watch consoles."
+      echo ""
+
+      [ ! -f "$backupDir/internal_flash_backup_${DEVICE}.bin" ] || [ ! -f "$backupDir/flash_backup_${DEVICE}.bin" ] && errorMsg
+
+      [ ! -f "$cfwDir/flash_backup_${DEVICE}.bin" ] && cp "$backupDir/flash_backup_${DEVICE}.bin" "$cfwDir/flash_backup_${DEVICE}.bin"
+      [ ! -f "$cfwDir/internal_flash_backup_${DEVICE}.bin" ] && cp "$backupDir/internal_flash_backup_${DEVICE}.bin" "$cfwDir/internal_flash_backup_${DEVICE}.bin"
+
+      cd game-and-watch-patch || exit
+
+      make clean
+      OPENOCD="/opt/openocd-git/bin/openocd"
+      [ "$DEVICE" == "mario" ] && make PATCH_PARAMS="--device=mario --internal-only" ADAPTER="$ADAPTER" flash_patched
+
+      if [ "$DEVICE" == "zelda" ]; then
+        [ "$CHIP" == "4" ] && make PATCH_PARAMS="--device=zelda --extended --no-la --no-sleep-images --extended" ADAPTER="$ADAPTER" flash
+        [ "$CHIP" -ge "64" ] && make PATCH_PARAMS="--device=zelda" ADAPTER="$ADAPTER" LARGE_FLASH=1 flash_patched
+      fi
+
+      cd ..
+      retroGo
+      exit
+      ;;
+    "Exit")
+      exit
+      ;;
+    *) echo "invalid option $REPLY" ;;
+    esac
+  done
+}
+
+chipSelectionMenu() {
+  echo ""
+  echo "Select the size of your flash chip.
+4 is the default:"
+  echo ""
+  chp=$(optionsDefaultValue "${chips[@]}")
+
+  chipSize "$chp"
+
+  echo ""
+  echo "You selected a $((CHIP + 4))Mb Flash Chip"
+}
+
+adapterSelectionMenu() {
+  echo ""
+  echo "You must configure this for the debug adapter you're using!
+stlink is the default:"
+  echo ""
+  adp=$(optionsDefaultValue "${adapters[@]}")
+
+  selectDebugger "$adp"
+
+  echo ""
+  echo "You choose $ADAPTER as your ARM debug probe"
+}
+
+deviceSelectionMenu() {
+  echo ""
+  echo "Please select your Game & Watch type.
+Mario is the default:"
+  echo ""
+
+  gw=$(optionsDefaultValue "${gwVersion[@]}")
+
+  selectGWType "$gw"
+  echo ""
+  echo "You choose $DEVICE as your Game & Watch"
+}
+
+if [ "$1" == "--h" ] || [[ ! ${adpArgs[*]} =~ $1 ]] || [[ ! ${devArg[*]} =~ $2 ]] || [[ ! ${chpArg[*]} =~ $3 ]]; then
+  helptext
+  exit 1
+fi
+
+if [ -n "${1}" ]; then
+  selectDebugger "${1}"
+fi
+
+if [ -n "${2}" ]; then
+  selectGWType "${2}" "${3}"
 fi
 
 if [ -n "${3}" ]; then
-  case "${3}" in
-  "4Mb")
-    CHIP="4Mb"
-    ;;
-  "64Mb")
-    CHIP="64Mb"
-    ;;
-  *)
-    exit
-    ;;
-  esac
-  echo ""
-  echo "You selected a $CHIP Flash Chip"
+  chipSize "${3}"
 fi
+echo ""
 
+[ -f "remove_me_after_first_run" ] && getRequirements
 
+[ -z "${DEVICE}" ] && deviceSelectionMenu
+[ -z "${ADAPTER}" ] && adapterSelectionMenu
+[ "$DEVICE" == "zelda" ] && [ -z "${CHIP}" ] && chipSelectionMenu
 
-echo "[ADAPTER]: $ADAPTER"
-echo "[DEVICE]: $DEVICE"
-echo "[CHIP]: $CHIP"
-
-# echo "$(($num1-$num2))"
-
-[ -z "${DEVICE}" ] && selectGWType
-[ -z "${ADAPTER}" ] && selectDebugger
-[ -z "${CHIP}" ] && chipSize
+[ -n "${DEVICE}" ] && [ -n "${ADAPTER}" ] && [ -n "${CHIP}" ] && toolsSelectionMenu
